0e5780d780f85bc619c579e01916621bbb831098
Usama Rahman <rahmanusama390@gmail.com>
2023-10-08 15:05:09 +0600

    w

---
+++ app.js
@@ -0,0 +1,8 @@
+const colors = ["green", "red", "rgba(133,145,200)" , "#f1f2f3"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    
+})
